374724e9df9658b8d6752cf9ee1c576bc5b473ac
Serj <mrnobody959@gmail.com>
2022-08-01 14:46:14 +0300

    cursor pointer fix

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -20,6 +20,7 @@ export const OptionButton = styled.button`
   border-radius: 5px;
   background-color: red;
   box-shadow: 0px 5px 10px -3px rgba(0,0,0,0.1);
+  cursor: pointer;
   transition: all 250ms;
 
   &:not(:last-child) {
